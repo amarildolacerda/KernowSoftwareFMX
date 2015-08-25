be451ebf6f47c3782dfe94edfb40a3a931446fa8
Graham Murt <graham@kernow-software.co.uk>
2015-08-25 10:45:04 +0100

    fix
    - fixed android delete button which could appear for row 0 randomly

--- Demos/ksComponentsDemo.dpr
+++ Demos/ksComponentsDemo.dpr
@@ -3,7 +3,8 @@ program ksComponentsDemo;
 uses
   System.StartUpCopy,
   FMX.Forms,
-  untMain in 'untMain.pas' {Form6};
+  untMain in 'untMain.pas' {Form6},
+  FMX.ListView in 'c:\program files (x86)\embarcadero\studio\16.0\source\fmx\FMX.ListView.pas';
 
 {$R *.res}
 
--- ksListView.pas
+++ ksListView.pas
@@ -2741,6 +2741,8 @@ procedure TksListView.MouseDown(Button: TMouseButton; Shift: TShiftState;
 var
   ARow: TKsListItemRow;
 begin
+  if y < 0 then
+    Exit;
   inherited;
   FMouseDownPos := PointF(x-ItemSpaces.Left, y);
   FMouseDownTime := Now;
@@ -2751,6 +2753,8 @@ end;
 
 procedure TksListView.MouseMove(Shift: TShiftState; X, Y: Single);
 begin
+  if y < 0 then
+    Exit;
   inherited;
   FCurrentMousepos := PointF(x-ItemSpaces.Left, y);
 end;
@@ -2764,8 +2768,9 @@ var
   AMouseDownRect: TRectF;
   ARowRect: TRectF;
 begin
+  if y < 0 then
+    Exit;
   inherited;
-
   AMouseDownRect := RectF(FMouseDownPos.X-8, FMouseDownPos.Y-8, FMouseDownPos.X+8, FMouseDownPos.Y+8);
   x := x - ItemSpaces.Left;
   if PtInRect(AMouseDownRect, PointF(x, y)) then
@@ -2792,7 +2797,7 @@ begin
       if (FKeepSelection = False) and (ItemIndex > -1) then
       begin
         Sleep(100);
-        ItemIndex := -1;
+        //ItemIndex := -1;
         Application.ProcessMessages;
       end;
       ARow.ProcessClick;
@@ -2840,7 +2845,7 @@ begin
   // remove row selection?
   if (FKeepSelection = False) and (ItemIndex > -1) then
   begin
-    ItemIndex := -1;
+    //ItemIndex := -1;
     Application.ProcessMessages;
   end;
 end;
