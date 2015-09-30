c68e75681cff563af46026369964e9015c8f874f
Graham Murt <graham@kernow-software.co.uk>
2015-09-30 20:38:08 +0100

    Update
    - fix for row object clicking

--- ksListView.pas
+++ ksListView.pas
@@ -1,4 +1,84 @@
-﻿{*******************************************************************************
+﻿{$A8,B-,C+,D+,E-,F-,G+,H+,I+,J-,K-,L+,M-,N-,O+,P+,Q-,R-,S-,T-,U-,V+,W-,X+,Y+,Z1}
+{$MINSTACKSIZE $00004000}
+{$MAXSTACKSIZE $00100000}
+{$IMAGEBASE $00400000}
+{$APPTYPE GUI}
+{$WARN SYMBOL_DEPRECATED ON}
+{$WARN SYMBOL_LIBRARY ON}
+{$WARN SYMBOL_PLATFORM ON}
+{$WARN SYMBOL_EXPERIMENTAL ON}
+{$WARN UNIT_LIBRARY ON}
+{$WARN UNIT_PLATFORM ON}
+{$WARN UNIT_DEPRECATED ON}
+{$WARN UNIT_EXPERIMENTAL ON}
+{$WARN HRESULT_COMPAT ON}
+{$WARN HIDING_MEMBER ON}
+{$WARN HIDDEN_VIRTUAL ON}
+{$WARN GARBAGE ON}
+{$WARN BOUNDS_ERROR ON}
+{$WARN ZERO_NIL_COMPAT ON}
+{$WARN STRING_CONST_TRUNCED ON}
+{$WARN FOR_LOOP_VAR_VARPAR ON}
+{$WARN TYPED_CONST_VARPAR ON}
+{$WARN ASG_TO_TYPED_CONST ON}
+{$WARN CASE_LABEL_RANGE ON}
+{$WARN FOR_VARIABLE ON}
+{$WARN CONSTRUCTING_ABSTRACT ON}
+{$WARN COMPARISON_FALSE ON}
+{$WARN COMPARISON_TRUE ON}
+{$WARN COMPARING_SIGNED_UNSIGNED ON}
+{$WARN COMBINING_SIGNED_UNSIGNED ON}
+{$WARN UNSUPPORTED_CONSTRUCT ON}
+{$WARN FILE_OPEN ON}
+{$WARN FILE_OPEN_UNITSRC ON}
+{$WARN BAD_GLOBAL_SYMBOL ON}
+{$WARN DUPLICATE_CTOR_DTOR ON}
+{$WARN INVALID_DIRECTIVE ON}
+{$WARN PACKAGE_NO_LINK ON}
+{$WARN PACKAGED_THREADVAR ON}
+{$WARN IMPLICIT_IMPORT ON}
+{$WARN HPPEMIT_IGNORED ON}
+{$WARN NO_RETVAL ON}
+{$WARN USE_BEFORE_DEF ON}
+{$WARN FOR_LOOP_VAR_UNDEF ON}
+{$WARN UNIT_NAME_MISMATCH ON}
+{$WARN NO_CFG_FILE_FOUND ON}
+{$WARN IMPLICIT_VARIANTS ON}
+{$WARN UNICODE_TO_LOCALE ON}
+{$WARN LOCALE_TO_UNICODE ON}
+{$WARN IMAGEBASE_MULTIPLE ON}
+{$WARN SUSPICIOUS_TYPECAST ON}
+{$WARN PRIVATE_PROPACCESSOR ON}
+{$WARN UNSAFE_TYPE OFF}
+{$WARN UNSAFE_CODE OFF}
+{$WARN UNSAFE_CAST OFF}
+{$WARN OPTION_TRUNCATED ON}
+{$WARN WIDECHAR_REDUCED ON}
+{$WARN DUPLICATES_IGNORED ON}
+{$WARN UNIT_INIT_SEQ ON}
+{$WARN LOCAL_PINVOKE ON}
+{$WARN MESSAGE_DIRECTIVE ON}
+{$WARN TYPEINFO_IMPLICITLY_ADDED ON}
+{$WARN RLINK_WARNING ON}
+{$WARN IMPLICIT_STRING_CAST ON}
+{$WARN IMPLICIT_STRING_CAST_LOSS ON}
+{$WARN EXPLICIT_STRING_CAST OFF}
+{$WARN EXPLICIT_STRING_CAST_LOSS OFF}
+{$WARN CVT_WCHAR_TO_ACHAR ON}
+{$WARN CVT_NARROWING_STRING_LOST ON}
+{$WARN CVT_ACHAR_TO_WCHAR ON}
+{$WARN CVT_WIDENING_STRING_LOST ON}
+{$WARN NON_PORTABLE_TYPECAST ON}
+{$WARN XML_WHITESPACE_NOT_ALLOWED ON}
+{$WARN XML_UNKNOWN_ENTITY ON}
+{$WARN XML_INVALID_NAME_START ON}
+{$WARN XML_INVALID_NAME ON}
+{$WARN XML_EXPECTED_CHARACTER ON}
+{$WARN XML_CREF_NO_RESOLVE ON}
+{$WARN XML_NO_PARM ON}
+{$WARN XML_NO_MATCHING_PARM ON}
+{$WARN IMMUTABLE_STRINGS OFF}
+{*******************************************************************************
 *                                                                              *
 *  TksListView - High-Performance Mobile ListView Component                    *
 *                                                                              *
@@ -2015,6 +2095,8 @@ begin
   Result.Text := AText;
   ShowAccessory := False;
   FList.Add(Result);
+  CanSelect := False;
+  Changed;
 end;
 
 
@@ -3015,6 +3097,7 @@ procedure TksListView.MouseDown(Button: TMouseButton; Shift: TShiftState;
   x, y: single);
 var
   ARow: TKsListItemRow;
+  ARowRect: TRectF;
 begin
   ARow := GetRowFromYPos(y);
   if (y < 0) or (ARow = nil) then
@@ -3025,6 +3108,11 @@ begin
   if ARow.CanSelect = False then
     ItemIndex := -1;
   inherited;
+
+  ARowRect := GetItemRect(ARow.Index);
+  FClickedRowObj := RowObjectAtPoint(ARow, x, y - ARowRect.Top);
+  if FClickedRowObj <> nil then
+    FClickedRowObj.MouseDown;
 
   if (Button = TMouseButton.mbRight) then
     ItemIndex := ARow.Index;
@@ -3078,6 +3166,8 @@ begin
     ARowRect := GetItemRect(ARow.Index);
 
     FClickedRowObj := RowObjectAtPoint(ARow, x, y - ARowRect.Top);
+    if FClickedRowObj <> nil then
+      FClickedRowObj.MouseUp;
     AId := ARow.ID;
 
     if (MilliSecondsBetween(FMouseDownTime, Now) >= 500) and (Assigned(FOnLongClick)) then
@@ -3386,7 +3476,7 @@ end;
 function TksListItemRowButton.Render(ACanvas: TCanvas): Boolean;
 begin
   inherited Render(ACanvas);
-  DrawButton(ACanvas, Rect, FText, False, FTintColor, ksButtonDefault);
+  DrawButton(ACanvas, Rect, FText, FState = Pressed, FTintColor, ksButtonDefault);
   Result := True;
 end;
 
